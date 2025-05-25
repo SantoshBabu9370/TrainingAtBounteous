2ca2eb2d1bd9420941c1bcbe809ae1502de93f65
Santosh Babu <santosh.babu@bounteous.com>
2025-05-25 19:36:44 +0530

    Uploded

--- 23-05-2025_Day1/nextGreater.java
+++ 23-05-2025_Day1/nextGreater.java
@@ -6,7 +6,8 @@ public class nextGreater {
         char[] arr=Integer.toString(n).toCharArray();
         int i=arr.length-2;
        while(i>=0&&arr[i]>=arr[i+1]) i--;
-
+       //Hiiee
+    
         if(i==-1) return -1;
         int j=arr.length-1;
 
