2c6c64fc7e51540e447cda0d877b03e926f330bb
Santosh Babu <santosh.babu@bounteous.com>
2025-05-25 22:45:37 +0530

    Update NextGreaterElement

---
+++ 23-05-2025_Day1/NextGreaterElement.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class NextGreaterElement {
+    public static void findNextGreater(int[] nums) {
+        int len=nums.length;
+        int[] res=new int[len];
+        Stack<Integer> stk = new Stack<>();
+        for (int i=len-1;i>=0;i--){
+            while (!stk.isEmpty() && stk.peek()<=nums[i]) stk.pop();
+            res[i] = stk.isEmpty()?-1:stk.peek();
+            stk.push(nums[i]);
+        }
+        for(int n:res) System.out.print(n + " ");
+    }
+    public static void main(String[] args) {
+        int[] data = {4, 5, 2, 25, 7, 8};
+        findNextGreater(data);
+    }
+}
