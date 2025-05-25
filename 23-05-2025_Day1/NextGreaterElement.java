165de5aa0da6a07e65406cef0298f0fc33dcffb6
Santosh Babu <santosh.babu@bounteous.com>
2025-05-26 00:13:38 +0530

    Adding valid parenthesis

--- 23-05-2025_Day1/NextGreaterElement.java
+++ 23-05-2025_Day1/NextGreaterElement.java
@@ -12,7 +12,7 @@ public class NextGreaterElement {
         for(int n:res) System.out.print(n + " ");
     }
     public static void main(String[] args) {
-        int[] data = {4, 5, 2, 25, 7, 8};
+        int[] data = {1,3,2,4};
         findNextGreater(data);
     }
 }
--- 26-05-2025_Day2/hi.java
+++
@@ -1,7 +0,0 @@
-//package 26-05-2025_Day2;
-
-public class hi {
-    public static void main(String[] args) {
-        System.out.println("ji");
-    }
-}
